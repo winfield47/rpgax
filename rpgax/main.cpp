3be53b88e98aceb7876944799e888b425e0b9d82
Winfield202 <99300401+Winfield202@users.noreply.github.com>
2024-01-11 23:35:53 -0800

    Initial Commit

---
+++ rpgax/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rpgax
+//
+//  Created by Kyle Winfield Burnham on 1/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
