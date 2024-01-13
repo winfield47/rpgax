f12b5bf87c2819fe658b61eb1365e6302d404fe9
Winfield202 <99300401+Winfield202@users.noreply.github.com>
2024-01-13 12:49:40 -0800

    more commit/push testing

--- rpgax/main.cpp
+++ rpgax/main.cpp
@@ -6,10 +6,16 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
+    string input;
+    cout << "Please enter a number: ";
+    cin >> input;
     
+    cout << "Your number is: " << input << endl;
     
     return 0;
 }
