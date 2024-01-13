846920e497b50c539bdd1c58e3a02db62000d5ea
Winfield202 <99300401+Winfield202@users.noreply.github.com>
2024-01-12 21:28:33 -0800

    removed README.txt

--- rpgax/main.cpp
+++ rpgax/main.cpp
@@ -7,15 +7,9 @@
 
 #include <iostream>
 
-<<<<<<< HEAD
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, bro!\n";
-=======
 int main()
 {
     
     
->>>>>>> 11241309a249fb106a746983d928ccd879f12a8f
     return 0;
 }
