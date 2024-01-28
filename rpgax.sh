08c1e3710dd491526c901f025d99a8a2a19ba11c
Winfield202 <99300401+Winfield202@users.noreply.github.com>
2024-01-27 17:57:08 -0800

    added script to run rpgax in terminal

---
+++ rpgax.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Compile C++ files
+cd src/rpgax/
+g++ -o ../rpgax.out -std=c++11 core/WeaponMove.cpp core/Weapon.cpp core/Character.cpp core/Player.cpp core/Enemy.cpp utils/Utils.cpp utils/Game.cpp utils/MainLoop.cpp main.cpp
+cd ../../
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    # Run the generated a.out file
+    src/./rpgax.out
+else
+    echo "Compilation failed. Exiting."
+fi
+
